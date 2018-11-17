87e3b4c369876511cdc361530f83b2a1276d241b
PerezEnric <enriic19@gmail.com>
2018-11-17 19:09:29 +0100

    Brodilfer deleted from blit function

--- Game-Exercise/Motor2D/j1Render.cpp
+++ Game-Exercise/Motor2D/j1Render.cpp
@@ -135,7 +135,6 @@ void j1Render::ResetViewPort()
 // Blit to screen
 bool j1Render::Blit(SDL_Texture* texture, int x, int y, const SDL_Rect* section, float speed, double angle, int pivot_x, int pivot_y) const
 {
-	BROFILER_CATEGORY("j1RenderBlit", Profiler::Color::Silver);
 	bool ret = true;
 	uint scale = App->win->GetScale()*3;
 
